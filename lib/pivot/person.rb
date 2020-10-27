79429364468cc062017eb9e9d8e404d2f2220394
jayson <mjmcamba@gmail.com>
2020-10-28 00:41:27 +0800

    passing tests. TODO: ask about the expectations in the track_pivot_spec

--- lib/pivot/person.rb
+++ lib/pivot/person.rb
@@ -1,5 +1,24 @@
 module Pivot
   class Person
-    # Code here!
+
+    attr_accessor :email, :first_name, :last_name
+    attr_reader :items
+
+    def initialize params = {}
+      params.each { |key, value| send "#{key}=", value }
+    end
+
+    def items
+      @items ||= []
+    end
+
+    # would like to define interface/manager for adding the items to person
+    # and other operations for persons and items
+    def add_item(item)
+      return false unless item.is_a?(Item)
+      item.assignee = email
+      items.push(item)
+      yield if block_given?
+    end
   end
 end
--- lib/pivot/tracker.rb
+++ lib/pivot/tracker.rb
@@ -1,5 +1,33 @@
 module Pivot
   class Tracker
-    # Code here!
+    class << self
+      def count(items)
+        return 0 unless items.is_a?(Array)
+        items.count
+      end
+  
+      def item_for(items, assignee)
+        items_for_assignee(items, assignee).last
+      end
+
+      def pivoted?(items, assignee)
+        items_for_assignee(items, assignee).any?
+      end
+
+      def total_points(items, **options)
+        assignee = options[:assignee]
+        line_items = assignee.nil? ? unique_assignees(items) : items_for_assignee(items, options[:assignee])
+        line_items.sum{ |item| item[:points] }
+      end
+
+      def unique_assignees(items)
+        items.uniq {|item| item[:assignee]}
+      end
+
+      def items_for_assignee(items, assignee = nil)
+        return items unless assignee
+        items.select{|item| item[:assignee] == assignee}
+      end
+    end
   end
 end
